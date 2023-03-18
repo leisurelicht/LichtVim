f3c563b178e592be7502ff521b027d6fa0ac03a0
暮晨 <leisurelicht@gmail.com>
2023-03-19 00:59:38 +0800

    feat: 🎸 增加自动命令

--- lua/lichtvim/config/autocmds.lua
+++ lua/lichtvim/config/autocmds.lua
@@ -59,3 +59,29 @@ api.autocmd(
     end
   }
 )
+
+-- resize splits if window got resized
+api.autocmd(
+  {"VimResized"},
+  {
+    group = api.augroup("resize_splits"),
+    callback = function()
+      vim.cmd("tabdo wincmd =")
+    end
+  }
+)
+
+-- go to last loc when opening a buffer
+api.autocmd(
+  "BufReadPost",
+  {
+    group = api.augroup("last_loc"),
+    callback = function()
+      local mark = vim.api.nvim_buf_get_mark(0, '"')
+      local lcount = vim.api.nvim_buf_line_count(0)
+      if mark[1] > 0 and mark[1] <= lcount then
+        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      end
+    end
+  }
+)
