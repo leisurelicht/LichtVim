cf10ca8c27239bdc3cfc03c8d9255e9c7aacfd8e
暮晨 <leisurelicht@gmail.com>
2023-03-25 18:59:33 +0800

    feat: 🎸 update lualine

--- lua/lichtvim/plugins/lualine.lua
+++ lua/lichtvim/plugins/lualine.lua
@@ -1,3 +1,4 @@
+local icons_g = require("lichtvim.utils.ui.icons").git
 local icons_d = require("lichtvim.utils.ui.icons").diagnostics
 
 local function window_num()
@@ -10,17 +11,73 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {"gitsigns.nvim"},
   config = function()
+
+    local function fg(name)
+      return function()
+        ---@type {foreground?:number}?
+        local hl = vim.api.nvim_get_hl_by_name(name, true)
+        return hl and hl.foreground and
+                   {fg = string.format("#%06x", hl.foreground)}
+      end
+    end
+
     require("lualine").setup({
       options = {theme = "nightfox"},
       sections = {
         lualine_a = {
           -- {"tabs", separator = {right = ""}},
-          {window_num, separator = {right = ""}, color = {fg = "white", bg = "grey"}},
-          {"mode", fmt = function(str) return str:sub(1, 1) end}
+          {
+            window_num,
+            separator = {right = ""},
+            color = {fg = "white", bg = "grey"}
+          },
+          {
+            "mode",
+            fmt = function(str) return str:sub(1, 1) end,
+            separator = {right = ""}
+          },
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function()
+              return package.loaded["noice"] and
+                         require("noice").api.status.mode.has()
+            end,
+            separator = {right = ""}
+          }
+
         },
         lualine_c = {{vim.fn.getcwd()}, "filename"},
-        lualine_x = {"encoding", "fileformat", "filetype"},
+        lualine_x = {
+          {"encoding"},
+          {"fileformat"},
+          {"filetype"},
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and
+                         require("noice").api.status.command.has()
+            end,
+            color = fg("Statement")
+          },
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = fg("Special")
+          }
+        },
         lualine_y = {
+          {
+            "diff",
+            symbols = {
+              added = icons_g.added,
+              modified = icons_g.modified,
+              removed = icons_g.removed
+            },
+            separator = {left = ""},
+            color = {fg = "white", bg = "black"}
+          },
           {
             "diagnostics",
             -- Table of diagnostic sources, available sources are:
@@ -46,7 +103,7 @@ return {
             colored = true, -- Displays diagnostics status in color if set to true.
             update_in_insert = false, -- Update diagnostics in insert mode.
             always_visible = false, -- Show diagnostics even if there are none.
-            separator = {left = ""}
+            separator = {left = ""}
           },
           {"progress", separator = {left = ""}}
         }
@@ -54,7 +111,11 @@ return {
       inactive_sections = {
         lualine_a = {
           -- {"tabs", separator = {right = ""}},
-          {window_num, separator = {right = ""}, color = {fg = "white", bg = "grey"}}
+          {
+            window_num,
+            separator = {right = ""},
+            color = {fg = "white", bg = "grey"}
+          }
         }
       },
       extensions = {"nvim-tree", "symbols-outline", "fzf"}
