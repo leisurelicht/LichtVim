2600ce482e15e3f40d2f3e44aa3796d39221b148
暮晨 <leisurelicht@gmail.com>
2023-03-30 16:43:08 +0800

    refactor: 💡 format

--- lua/lichtvim/config/init.lua
+++ lua/lichtvim/config/init.lua
@@ -4,11 +4,10 @@
 -- =================
 --
 --
-local M = {}
-
 require("lichtvim.utils.G")
+local icons = require("lichtvim.utils.ui.icons")
 
-icons = require("lichtvim.utils.ui.icons")
+local M = {}
 
 M.lazy_version = ">=9.1.0"
 
@@ -21,14 +20,14 @@ local defaults = {
   defaults = {
     autocmds = true, -- lichtvim.config.autocmds
     keymaps = true, -- lichtvim.config.keymaps
-    options = true -- lichtvim.config.options
+    options = true, -- lichtvim.config.options
   },
   -- icons used by other plugins
   icons = {
     diagnostics = icons.diagnostics,
     git = icons.git,
-    kinds = icons.kinds
-  }
+    kinds = icons.kinds,
+  },
 }
 
 local options
@@ -37,46 +36,42 @@ function M.setup(opts)
   options = vim.tbl_deep_extend("force", defaults, opts or {})
   if not M.has() then
     require("lazy.config.util").error(
-      "**LichtVim** needs **lazy.nvim** version " ..
-        M.lazy_version .. " to work properly.\n" .. "Please upgrade **lazy.nvim**",
-      {title = "LichtVim"}
+      "**LichtVim** needs **lazy.nvim** version "
+        .. M.lazy_version
+        .. " to work properly.\n"
+        .. "Please upgrade **lazy.nvim**",
+      { title = "LichtVim" }
     )
     error("Exiting")
   end
 
   if vim.fn.argc(-1) == 0 then
-    api.autocmd(
-      "User",
-      {
-        group = api.augroup("LichtVim"),
-        pattern = "VeryLazy",
-        callback = function()
-          M.load("autocmds")
-          M.load("keymaps")
-        end
-      }
-    )
+    api.autocmd("User", {
+      group = api.augroup("LichtVim"),
+      pattern = "VeryLazy",
+      callback = function()
+        M.load("autocmds")
+        M.load("keymaps")
+      end,
+    })
   else
     M.load("autocmds")
     M.load("keymaps")
   end
 
-  require("lazy.core.util").try(
-    function()
-      if type(M.colorscheme) == "function" then
-        M.colorscheme()
-      else
-        vim.cmd.colorscheme(M.colorscheme)
-      end
+  require("lazy.core.util").try(function()
+    if type(M.colorscheme) == "function" then
+      M.colorscheme()
+    else
+      vim.cmd.colorscheme(M.colorscheme)
+    end
+  end, {
+    msg = "Could not load your colorscheme",
+    on_error = function(msg)
+      require("lazy.core.util").error(msg)
+      vim.cmd.colorscheme("nightfox")
     end,
-    {
-      msg = "Could not load your colorscheme",
-      on_error = function(msg)
-        require("lazy.core.util").error(msg)
-        vim.cmd.colorscheme("nightfox")
-      end
-    }
-  )
+  })
 end
 
 function M.has(range)
@@ -88,21 +83,18 @@ end
 function M.load(name)
   local Util = require("lazy.core.util")
   local function _load(mod)
-    Util.try(
-      function()
-        require(mod)
-      end,
-      {
-        msg = "Failed loading " .. mod,
-        on_error = function(msg)
-          local info = require("lazy.core.cache").find(mod)
-          if info == nil or (type(info) == "table" and #info == 0) then
-            return
-          end
-          Util.error(msg)
+    Util.try(function()
+      require(mod)
+    end, {
+      msg = "Failed loading " .. mod,
+      on_error = function(msg)
+        local info = require("lazy.core.cache").find(mod)
+        if info == nil or (type(info) == "table" and #info == 0) then
+          return
         end
-      }
-    )
+        Util.error(msg)
+      end,
+    })
   end
 
   -- always load lichtvim, then user file
@@ -126,17 +118,14 @@ function M.init()
   end
 end
 
-setmetatable(
-  M,
-  {
-    __index = function(_, key)
-      if options == nil then
-        return vim.deepcopy(defaults)[key]
-      end
-      ---@cast options LichtVimConfig
-      return options[key]
+setmetatable(M, {
+  __index = function(_, key)
+    if options == nil then
+      return vim.deepcopy(defaults)[key]
     end
-  }
-)
+    ---@cast options LichtVimConfig
+    return options[key]
+  end,
+})
 
 return M
