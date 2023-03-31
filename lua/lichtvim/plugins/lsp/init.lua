d0a1bf989d6738f0e1db8202b01c5e2d1d921a6e
暮晨 <leisurelicht@gmail.com>
2023-03-31 14:32:44 +0800

    feat: 🎸 update

--- lua/lichtvim/plugins/lsp/init.lua
+++ lua/lichtvim/plugins/lsp/init.lua
@@ -186,6 +186,34 @@ return {
     end,
   },
   {
+    "L3MON4D3/LuaSnip",
+    build = (not jit.os:find("Windows"))
+        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+    -- stylua: ignore
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true, silent = true, mode = "i",
+      },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     version = false,
     event = "InsertEnter",
@@ -193,7 +221,7 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      -- "saadparwaiz1/cmp_luasnip",
+      "saadparwaiz1/cmp_luasnip",
     },
     opts = function()
       local cmp = require("cmp")
