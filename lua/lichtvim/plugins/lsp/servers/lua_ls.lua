c56fb842c199e89b62b22b951caf611201f9022a
暮晨 <leisurelicht@gmail.com>
2023-04-09 02:58:18 +0800

    feat: 🎸 change lua_ls config

--- lua/lichtvim/plugins/lsp/servers/lua_ls.lua
+++ lua/lichtvim/plugins/lsp/servers/lua_ls.lua
@@ -27,7 +27,7 @@ return {
         -- },
         workspace = {
           -- library = vim.api.nvim_get_runtime_file("", true),
-          library = checkThirdParty,
+          library = "checkThirdParty",
         },
         telemetry = {
           enable = false,
