6fd2f35f87f04c1f1199bbcce9dedfee6c674ec0
暮晨 <leisurelicht@gmail.com>
2023-04-08 18:45:30 +0800

    fix: 🐛 cmp capabilities
    ✅ Closes: g

--- lua/lichtvim/plugins/lsp/init.lua
+++ lua/lichtvim/plugins/lsp/init.lua
@@ -176,7 +176,7 @@ return {
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         if lazy.has("cmp_nvim_lsp") then
-          capabilities = vim.lsp.protocol.make_client_capabilities(capabilities)
+          capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
         end
         options = vim.tbl_deep_extend("force", { capabilities = capabilities }, options or {})
 
