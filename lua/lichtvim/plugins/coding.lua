26c0041e0fb279cc81941753990e5e948036762f
暮晨 <leisurelicht@gmail.com>
2023-04-09 17:28:15 +0800

    feat: 🎸 替换 autopairs

--- lua/lichtvim/plugins/coding.lua
+++ lua/lichtvim/plugins/coding.lua
@@ -3,8 +3,16 @@ return {
   { "p00f/nvim-ts-rainbow", event = { "BufRead", "BufNewFile" } },
   { -- 自动配对
     "windwp/nvim-autopairs",
+    enabled = true,
     event = "InsertEnter",
     opts = { enable_check_bracket_line = false, ignored_next_char = "[%w%.]" },
+  },
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
   },
   {
     "andymass/vim-matchup",
