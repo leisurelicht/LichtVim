209e5e1916b8df6847f3bb97262ac819dc14b790
暮晨 <leisurelicht@gmail.com>
2023-04-10 20:48:22 +0800

    feat: lsp hover and signatore 样式增加兜底方案

--- lua/lichtvim/plugins/lsp/init.lua
+++ lua/lichtvim/plugins/lsp/init.lua
@@ -13,6 +13,26 @@ local function lsmod(modname, fn)
   end)
 end
 
+-- 为 lsp hover 添加文件类型
+local function lsp_hover(_, result, ctx, config)
+  -- Add file type for LSP hover
+  local bufnr, winner = vim.lsp.handlers.hover(_, result, ctx, config)
+  if bufnr and winner then
+    vim.api.nvim_buf_set_option(bufnr, "filetype", config.filetype)
+    return bufnr, winner
+  end
+end
+
+-- 为 lsp 签名帮助添加文件类型
+local function lsp_signature_help(_, result, ctx, config)
+  -- Add file type for LSP signature help
+  local bufnr, winner = vim.lsp.handlers.signature_help(_, result, ctx, config)
+  if bufnr and winner then
+    vim.api.nvim_buf_set_option(bufnr, "filetype", config.filetype)
+    return bufnr, winner
+  end
+end
+
 local function list_or_jump(title)
   local api = vim.api
   local util = vim.lsp.util
@@ -172,10 +192,24 @@ return {
           ["textDocument/typeDefinition"] = list_or_jump("LSP Type Definitions"),
           ["textDocument/implementation"] = list_or_jump("LSP Implementations"),
         }
+
+        -- 设置浮动样式(兜底方案)
+        local lsp_handlers = {
+          ["textDocument/hover"] = vim.lsp.with(lsp_hover, {
+            border = "rounded",
+            filetype = "lsp-hover",
+          }),
+          ["textDocument/signatureHelp"] = vim.lsp.with(lsp_signature_help, {
+            border = "rounded",
+            filetype = "lsp-signature-help",
+          }),
+        }
+
         if settings.document_diagnostics ~= nil and not settings.document_diagnostics then
           handler["textDocument/publishDiagnostics"] = function(...) end
+          options.handlers = vim.tbl_deep_extend("force", handler, options.handlers or {})
         end
-        options.handlers = vim.tbl_deep_extend("force", handler, options.handlers or {})
+        options.handlers = vim.tbl_extend("force", lsp_handlers, options.handlers or {})
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         if lazy.has("cmp_nvim_lsp") then
