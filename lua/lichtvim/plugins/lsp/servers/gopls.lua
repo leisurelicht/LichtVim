522626038c5d6c3762087ef9d623b54253c25b23
暮晨 <leisurelicht@gmail.com>
2023-04-11 23:03:12 +0800

    refactor: 💡 use gopls default config values

--- lua/lichtvim/plugins/lsp/servers/gopls.lua
+++ lua/lichtvim/plugins/lsp/servers/gopls.lua
@@ -1,18 +1,8 @@
--- local wk = require("which-key")
-local util = require("lspconfig.util")
-
 return {
   settings = {
     document_diagnostics = true,
     document_formatting = true,
     formatting_on_save = false,
   },
-  options = {
-    cmd = { "gopls" },
-    single_file_support = true,
-    filetypes = { "go", "gomod", "gotmpl" },
-    root_dir = function(fname)
-      return util.root_pattern("go.work")(fname) or util.root_pattern("go.mod", ".git")(fname)
-    end,
-  },
+  options = {},
 }
