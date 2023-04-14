cc1b231216043235be730c7901a4047747cddc79
暮晨 <leisurelicht@gmail.com>
2023-04-14 16:00:13 +0800

    refactor: 💡 update

--- lua/lichtvim/plugins/coding/git.lua
+++ lua/lichtvim/plugins/coding/git.lua
@@ -1,4 +1,4 @@
-local function git_key(gs, bufnr)
+local function git_keymaps(gs, bufnr)
   map.set("n", "<leader>gB", "<cmd>GitBlameToggle<cr>", "Toggle line blame")
   map.set("n", "<leader>go", "<cmd>GitBlameOpenCommitURL<cr>", "Open commit url")
   map.set({ "n", "v" }, "<leader>ga", ":Gitsigns stage_hunk<cr>", "Add hunk", { buffer = bufnr })
@@ -7,6 +7,7 @@ local function git_key(gs, bufnr)
   map.set("n", "<leader>gR", gs.reset_buffer, "Reset buffer", { buffer = bufnr })
   map.set("n", "<leader>gu", gs.undo_stage_hunk, "Undo stage hunk", { buffer = bufnr })
   map.set("n", "<leader>gp", gs.preview_hunk, "Preview hunk", { buffer = bufnr })
+  map.set("n", "<leader>gt", gs.toggle_deleted, "Toggle deleted", { buffer = bufnr })
   map.set("n", "<leader>gb", function()
     gs.blame_line({ full = true })
   end, "Show blame line", { buffer = bufnr })
@@ -14,7 +15,6 @@ local function git_key(gs, bufnr)
   map.set("n", "<leader>gD", function()
     gs.diffthis("~")
   end, "Diff this?", { buffer = bufnr })
-  map.set("n", "<leader>gt", gs.toggle_deleted, "Toggle deleted", { buffer = bufnr })
 
   map.set("n", "]g", function()
     if vim.wo.diff then
@@ -56,34 +56,14 @@ return {
     config = function()
       require("gitsigns").setup({
         signs = {
-          add = {
-            hl = "GitSignsAdd",
-            text = "+",
-            numhl = "GitSignsAddNr",
-            linehl = "GitSignsAddLn",
-          },
-          change = {
-            hl = "GitSignsChange",
-            text = "!",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn",
-          },
-          delete = {
-            hl = "GitSignsDelete",
-            text = "-",
-            numhl = "GitSignsDeleteNr",
-            linehl = "GitSignsDeleteLn",
-          },
-          changedelete = {
-            hl = "GitSignsChange",
-            text = "~",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn",
-          },
+          add = { text = "+" },
+          change = { text = "!" },
+          delete = { text = "-" },
+          changedelete = { text = "~" },
         },
         on_attach = function(bufnr)
           local gs = package.loaded.gitsigns
-          git_key(gs, bufnr)
+          git_keymaps(gs, bufnr)
         end,
       })
     end,
