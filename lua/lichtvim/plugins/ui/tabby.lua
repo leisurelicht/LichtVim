af09bb6d4744c1e1f2d87947f1ab30d2a116818a
暮晨 <leisurelicht@gmail.com>
2023-04-18 00:12:16 +0800

    feat: 🎸 update tabby config

--- lua/lichtvim/plugins/ui/tabby.lua
+++ lua/lichtvim/plugins/ui/tabby.lua
@@ -1,5 +1,13 @@
 local str = require("lichtvim.utils").str
 
+local name_map = {
+  alpha = "Alpha",
+  checkhealth = "CheckHealth",
+  NvimTree = "File Explorer",
+  ["neo-tree"] = "File Explorer",
+  toggleterm = "Terminal",
+}
+
 return {
   {
     "nanozuki/tabby.nvim",
@@ -10,26 +18,13 @@ return {
         return {
           { { "  ", hl = "LichtTLHead" }, line.sep("", "LichtTLHead", "LichtTLLineSep") },
           line.tabs().foreach(function(tab)
-            local tab_name = tab.name()
+            -- if t_api.is_float_win(0) then
+            --   return
+            -- end
 
-            if t_api.is_float_win(0) then
-              return
-            end
-
-            local tnl = vim.fn.tolower(tab_name)
-            if str.starts_with(tnl, "nvimtree") or str.starts_with(tnl, "neo-tree") then
-              tab_name = "File Explorer"
-            end
-
-            if #tab.wins() == 0 then
-              local buf = tab.current_win().buf().id
-              local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-              if ft == "alpha" then
-                tab_name = "Alpha"
-              elseif ft == "checkhealth" then
-                tab_name = "CheckHealth"
-              end
-            end
+            local buf = tab.current_win().buf().id
+            local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+            local tab_name = name_map[ft] or tab.name()
 
             local hl = tab.is_current() and "LichtTLActiveTab" or "LichtTLUnActive"
             return {
@@ -45,23 +40,13 @@ return {
           end),
           line.spacer(),
           line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-            if t_api.is_float_win(0) then
-              return
-            end
+            -- if t_api.is_float_win(0) then
+            --   return
+            -- end
 
-            local win_name = win.buf_name()
-
-            local ft = vim.api.nvim_buf_get_option(0, "filetype")
-            if ft == "alpha" then
-              win_name = "Alpha"
-            elseif ft == "checkhealth" then
-              win_name = "CheckHealth"
-            end
-
-            local wnl = vim.fn.tolower(win_name)
-            if str.starts_with(wnl, "nvimtree") or str.starts_with(wnl, "neo-tree") then
-              win_name = "File Explorer"
-            end
+            local buf = win.buf().id
+            local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+            local win_name = name_map[ft] or win.buf_name()
 
             local hl = win.is_current() and "LichtTLActiveWin" or "LichtTLUnActive"
             return {
