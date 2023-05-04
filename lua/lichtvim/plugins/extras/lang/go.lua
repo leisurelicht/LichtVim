80a10e07dae304b4584771a0534aff9f88a70165
暮晨 <leisurelicht@gmail.com>
2023-05-04 17:08:59 +0800

    feat: 🎸 update

--- lua/lichtvim/plugins/extras/lang/go.lua
+++ lua/lichtvim/plugins/extras/lang/go.lua
@@ -11,8 +11,8 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        -- "gomodifytags",
-        -- "impl",
+        "gomodifytags",
+        "impl",
         -- "goimports",
       })
     end,
@@ -23,23 +23,23 @@ return {
       local null_ls = require("null-ls")
       vim.list_extend(opts.sources, {
         -- null_ls.builtins.formatting.goimports,
-        -- null_ls.builtins.code_actions.gomodifytags,
-        -- null_ls.builtins.code_actions.impl,
-        -- null_ls.builtins.code_actions.refactoring,
+        null_ls.builtins.code_actions.gomodifytags,
+        null_ls.builtins.code_actions.impl,
+        null_ls.builtins.code_actions.refactoring,
       })
     end,
   },
-  -- {
-  --   "ThePrimeagen/refactoring.nvim",
-  --   opts = {
-  --     prompt_func_return_type = {
-  --       go = true,
-  --     },
-  --     prompt_func_param_type = {
-  --       go = true,
-  --     },
-  --   },
-  -- },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    opts = {
+      prompt_func_return_type = {
+        go = true,
+      },
+      prompt_func_param_type = {
+        go = true,
+      },
+    },
+  },
   {
     "neovim/nvim-lspconfig",
     opts = {
