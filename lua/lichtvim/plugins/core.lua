7a0de334339565ec71a2907a4f67485e7362c10b
暮晨 <leisurelicht@gmail.com>
2023-05-04 14:21:39 +0800

    feat: 🎸 update

--- lua/lichtvim/plugins/core.lua
+++ lua/lichtvim/plugins/core.lua
@@ -1,13 +1,6 @@
 require("lichtvim.config").init()
 
-local dev = os.getenv("LICHTVIM_DEV")
-if dev == "1" then
-  return {
-    { "folke/lazy.nvim", version = "*" },
-  }
-else
-  return {
-    { "folke/lazy.nvim", version = "*" },
-    { "leisurelicht/LichtVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
-  }
-end
+return {
+  { "folke/lazy.nvim", version = "*" },
+  { "leisurelicht/LichtVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
+}
