3222b8bb052b1433783b7f75278706d8e53d27fb
暮晨 <leisurelicht@gmail.com>
2023-05-14 04:19:49 +0800

    feat: 🎸 update lualine config

--- lua/lichtvim/plugins/ui/lualine.lua
+++ lua/lichtvim/plugins/ui/lualine.lua
@@ -76,24 +76,9 @@ return {
           },
           lualine_y = {
             { "location" },
-            { "progress" },
           },
           lualine_z = {
-            {
-              "tabs",
-              max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-              mode = 0, -- 0: Shows tab_nr
-              use_mode_colors = true,
-              fmt = function(name, context)
-                -- Show + if buffer is modified in tab
-                local buflist = vim.fn.tabpagebuflist(context.tabnr)
-                local winnr = vim.fn.tabpagewinnr(context.tabnr)
-                local bufnr = buflist[winnr]
-                local mod = vim.fn.getbufvar(bufnr, "&mod")
-
-                return name .. (mod == 1 and " +" or "")
-              end,
-            },
+            { "progress" },
           },
         },
         inactive_sections = {
