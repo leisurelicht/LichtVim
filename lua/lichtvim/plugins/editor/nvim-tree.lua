76695cbe520c5780355231d1b31650d008267a48
暮晨 <leisurelicht@gmail.com>
2023-05-15 10:58:46 +0800

    feat: 🎸 nvim-tree autoclose

--- lua/lichtvim/plugins/editor/nvim-tree.lua
+++ lua/lichtvim/plugins/editor/nvim-tree.lua
@@ -12,6 +12,25 @@ local function open_nvim_tree_dir(data)
 end
 api.autocmd({ "VimEnter" }, { group = api.augroup("explorer"), callback = open_nvim_tree_dir })
 
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local invalid_win = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(invalid_win, w)
+      end
+    end
+    if #invalid_win == #wins - 1 then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(invalid_win) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end,
+})
+
 local function on_attach(bufnr)
   local api = require("nvim-tree.api")
 
@@ -23,16 +42,32 @@ local function on_attach(bufnr)
 
   vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New Tab"))
   vim.keymap.set("n", "s", api.node.open.vertical, opts("Open: Vertical Split"))
-  vim.keymap.set("n", "o", api.node.open.horizontal, opts("Open: Horizontal Split"))
-  -- vim.keymap.set("n", "<C-s>", api.node.run.system, opts("Run System"))
-  -- vim.keymap.set("n", "<C-o>", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "d", api.fs.trash, opts("Trash"))
 
   vim.keymap.set("n", "O", "", { buffer = bufnr })
   vim.keymap.del("n", "O", { buffer = bufnr })
+
+  vim.keymap.set("n", "D", "", { buffer = bufnr })
+  vim.keymap.del("n", "D", { buffer = bufnr })
+
+  vim.keymap.set("n", "]d", api.node.navigate.diagnostics.next, opts("Next Diagnostic"))
+  vim.keymap.set("n", "[d", api.node.navigate.diagnostics.prev, opts("Prev Diagnostic"))
+  vim.keymap.set("n", "]e", "", { buffer = bufnr })
+  vim.keymap.del("n", "]e", { buffer = bufnr })
+  vim.keymap.set("n", "[e", "", { buffer = bufnr })
+  vim.keymap.del("n", "[e", { buffer = bufnr })
+
+  vim.keymap.set("n", "[g", api.node.navigate.git.prev, opts("Prev Git"))
+  vim.keymap.set("n", "]g", api.node.navigate.git.next, opts("Next Git"))
+  vim.keymap.set("n", "[c", "", { buffer = bufnr })
+  vim.keymap.del("n", "[c", { buffer = bufnr })
+  vim.keymap.set("n", "]c", "", { buffer = bufnr })
+  vim.keymap.del("n", "]c", { buffer = bufnr })
+
   vim.keymap.set("n", "g?", "", { buffer = bufnr })
   vim.keymap.del("n", "g?", { buffer = bufnr })
   vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-  vim.keymap.set("n", "C", api.tree.change_root_to_node, opts("CD"))
+
   vim.keymap.set("n", "P", function()
     local node = api.tree.get_node_under_cursor()
     print(node.absolute_path)
@@ -62,8 +97,8 @@ return {
         respect_buf_cwd = true,
         update_focused_file = { enable = true, update_root = true },
         diagnostics = {
-          enable = false,
-          show_on_dirs = false,
+          enable = true,
+          show_on_dirs = true,
           icons = {
             hint = icons.Hint,
             info = icons.Info,
