e7ffdecf3c6826aa3221e1319b7cf4111a5eb868
暮晨 <leisurelicht@gmail.com>
2023-05-16 01:58:06 +0800

    feat: 🎸 use auto pair

--- lua/lichtvim/plugins/coding/autopairs.lua
+++ lua/lichtvim/plugins/coding/autopairs.lua
@@ -1,13 +1,13 @@
 return {
   { -- 自动配对
     "windwp/nvim-autopairs",
-    enabled = false,
+    enabled = true,
     event = { "BufNewFile", "BufRead" },
     opts = { enable_check_bracket_line = false, ignored_next_char = "[%w%.]" },
   },
   {
     "echasnovski/mini.pairs",
-    enabled = true,
+    enabled = false,
     event = { "BufNewFile", "BufRead" },
     config = function(_, opts)
       require("mini.pairs").setup(opts)
