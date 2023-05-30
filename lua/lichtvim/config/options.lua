50e108a89933eb30240c7fe8853e95815e08825d
暮晨 <leisurelicht@gmail.com>
2023-05-30 22:47:42 +0800

    fix: close python3 provider

--- lua/lichtvim/config/options.lua
+++ lua/lichtvim/config/options.lua
@@ -11,7 +11,7 @@ local sys = require("lichtvim.utils").sys
 --   vim.api.nvim_set_var("python3_host_prog", "python3")
 -- end
 
-vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
