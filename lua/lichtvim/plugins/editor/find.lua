29bdedf88fd73a7c4b94864060d2733ef8b194f9
暮晨 <leisurelicht@gmail.com>
2023-06-07 23:47:07 +0800

    删除注释代码

--- lua/lichtvim/plugins/editor/find.lua
+++ lua/lichtvim/plugins/editor/find.lua
@@ -141,21 +141,6 @@ return {
               ["<C-p>"] = actions.cycle_history_prev,
               ["<C-j>"] = actions.move_selection_next,
               ["<C-k>"] = actions.move_selection_previous,
-              -- ["<C-g>"] = function(prompt_bufnr)
-              --   -- Use nvim-window-picker to choose the window by dynamically attaching a function
-              --   local action_set = require("telescope.actions.set")
-              --   local action_state = require("telescope.actions.state")
-
-              --   local picker = action_state.get_current_picker(prompt_bufnr)
-              --   picker.get_selection_window = function(picker, entry)
-              --     local picked_window_id = require("window-picker").pick_window() or vim.api.nvim_get_current_win()
-              --     -- Unbind after using so next instance of the picker acts normally
-              --     picker.get_selection_window = nil
-              --     return picked_window_id
-              --   end
-
-              --   return action_set.edit(prompt_bufnr, "edit")
-              -- end,
             },
             n = {
               ["q"] = actions.close,
