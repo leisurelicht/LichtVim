12b945fb224e6b7c94417b7f491877d48814c4e3
leisurelicht <leisurelicht@gmail.com>
2023-06-19 01:21:10 +0800

    feat: update fold ui

--- lua/lichtvim/plugins/editor/enhance.lua
+++ lua/lichtvim/plugins/editor/enhance.lua
@@ -33,8 +33,8 @@ return {
             relculright = true,
             segments = {
               { text = { "%s" }, click = "v:lua.ScSa" },
-              { text = { builtin.lnumfunc, " " }, condition = { true, builtin.not_empty }, click = "v:lua.ScLa" },
-              { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+              { text = { "%l" }, click = "v:lua.ScLa" },
+              { text = { " ", builtin.foldfunc }, click = "v:lua.ScFa" },
             },
           })
         end,
