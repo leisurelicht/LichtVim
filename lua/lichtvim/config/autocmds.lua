b98d0c8f5068b158fea50c907a36608ac550e29d
leisurelicht <leisurelicht@gmail.com>
2023-06-20 00:35:54 +0800

    feat: add healthcheck to close with q

--- lua/lichtvim/config/autocmds.lua
+++ lua/lichtvim/config/autocmds.lua
@@ -44,6 +44,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "startuptime",
     "tsplayground",
     "noice",
+    "checkhealth",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
