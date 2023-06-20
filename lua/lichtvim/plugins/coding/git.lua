daeb4fb94eb44aecf2c0a0c67559dcbcdd31a24a
leisurelicht <leisurelicht@gmail.com>
2023-06-20 22:05:14 +0800

    feat: update statuscolumn ui

--- lua/lichtvim/plugins/coding/git.lua
+++ lua/lichtvim/plugins/coding/git.lua
@@ -21,11 +21,15 @@ return {
     dependencies = { "f-person/git-blame.nvim" },
     opts = {
       signs = {
-        add = { text = "+" },
-        change = { text = "!" },
-        delete = { text = "-" },
-        changedelete = { text = "~" },
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "│" },
+        topdelete = { text = "│" },
+        changedelete = { text = "│" },
+        untracked = { text = "┆" },
       },
+      signcolumn = true,
+      numhl = true,
       preview_config = {
         -- Options passed to nvim_open_win
         border = "rounded",
--- lua/lichtvim/plugins/editor/enhance.lua
+++ lua/lichtvim/plugins/editor/enhance.lua
@@ -32,16 +32,8 @@ return {
           require("statuscol").setup({
             relculright = true,
             segments = {
-              -- { text = { "%s" }, click = "v:lua.ScSa" },
-              -- {
-              --   sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
-              --   click = "v:lua.ScSa",
-              -- },
-              {
-                sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
-                click = "v:lua.ScSa",
-              },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+              { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+              { sign = { name = { "Git" }, maxwidth = 1, colwidth = 1, auto = false, wrap = false }, click = "v:lua.ScSa" },
               { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
             },
           })
