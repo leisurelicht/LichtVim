c36a04894dc5770bfbd7755964ea77e220109352
leisurelicht <leisurelicht@gmail.com>
2023-06-25 01:28:17 +0800

    feat: update find config

--- lua/lichtvim/plugins/editor/find.lua
+++ lua/lichtvim/plugins/editor/find.lua
@@ -1,16 +1,3 @@
-local function is_whitespace(line)
-  return vim.fn.match(line, [[^\s*$]]) ~= -1
-end
-
-local function all(tbl, check)
-  for _, entry in ipairs(tbl) do
-    if not check(entry) then
-      return false
-    end
-  end
-  return true
-end
-
 return {
   { "kkharji/sqlite.lua", lazy = true },
   {
@@ -33,33 +20,23 @@ return {
         lazy = true,
         dependencies = { { "sqlite.lua", module = "sqlite" } },
         opts = {
-          history = 1000,
           enable_persistent_history = true,
-          continuous_sync = true,
-          db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
           filter = function(data)
+            local function is_whitespace(line)
+              return vim.fn.match(line, [[^\s*$]]) ~= -1
+            end
+
+            local function all(tbl, check)
+              for _, entry in ipairs(tbl) do
+                if not check(entry) then
+                  return false
+                end
+              end
+              return true
+            end
+
             return not all(data.event.regcontents, is_whitespace)
           end,
-          keys = {
-            telescope = {
-              i = {
-                select = "<cr>",
-                paste = "<a-p>",
-                past_behind = "<a-o>",
-                replay = "<nop>",
-                delete = "<c-d>",
-                custom = {},
-              },
-              n = {
-                select = "<cr>",
-                paste = "p",
-                paste_behind = "P",
-                replay = "<nop>",
-                delete = "dd",
-                custom = {},
-              },
-            },
-          },
         },
       },
     },
