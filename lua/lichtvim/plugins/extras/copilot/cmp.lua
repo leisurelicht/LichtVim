9c3ef4460a90e187fa82d73dfa754e7d4b2ff651
leisurelicht <leisurelicht@gmail.com>
2023-07-11 02:14:28 +0800

    feat(copilot):copilot增加cmp补全

---
+++ lua/lichtvim/plugins/extras/copilot/cmp.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    dependencies = {
+      "leisurelicht/lualine-copilot.nvim",
+    },
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_y, 2, { "copilot" })
+    end,
+  },
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          require("lichtvim.utils.lazy").on_attach(function(client)
+            if client.name == "copilot" then
+              copilot_cmp._on_insert_enter({})
+            end
+          end)
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, { name = "copilot", group_index = 2 })
+      opts.sorting = opts.sorting or require("cmp.config.default")().sorting
+      table.insert(opts.sorting.comparators, 1, require("copilot_cmp.comparators").prioritize)
+    end,
+  },
+}
