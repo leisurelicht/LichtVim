7da073415febd84819fc330aedc415c325357506
leisurelicht <leisurelicht@gmail.com>
2023-07-12 01:01:33 +0800

    feat(catppuccin): match navic bg with lualine

--- lua/lichtvim/plugins/theme.lua
+++ lua/lichtvim/plugins/theme.lua
@@ -21,7 +21,7 @@ return {
             information = { "undercurl" },
           },
         },
-        navic = { enabled = true },
+        navic = { enabled = true, custom_bg = "lualine" },
         neotest = true,
         noice = true,
         notify = true,
