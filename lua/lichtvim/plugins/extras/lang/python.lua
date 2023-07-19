b861197f4da66472cffb6b6ce2895e7cef893141
leisurelicht <leisurelicht@gmail.com>
2023-07-20 00:36:30 +0800

    feat(python):add htmldjango to python filetypes

--- lua/lichtvim/plugins/extras/lang/python.lua
+++ lua/lichtvim/plugins/extras/lang/python.lua
@@ -1,16 +1,33 @@
+local python_filetypes = { "ninja", "python", "rst", "toml", "htmldjango" }
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+      vim.list_extend(opts.ensure_installed, python_filetypes)
     end,
   },
   {
     "linux-cultist/venv-selector.nvim",
     cmd = "VenvSelect",
     ft = "python",
+    keys = function()
+      local utils = require("lichtvim.utils")
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup(utils.title.add("Keymap"), { clear = false }),
+        pattern = { "*" },
+        callback = function(event)
+          if vim.fn.index(python_filetypes, vim.bo[event.buf].filetype) == -1 then
+            return
+          end
+
+          local opt = { buffer = event.buf, silent = true }
+          map.set("n", "<leader>cv", "<cmd>:VenvSelect<cr>", "Select VirtualEnv", opt)
+        end,
+      })
+    end,
     opts = {},
-    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+    config = true,
   },
   {
     "neovim/nvim-lspconfig",
