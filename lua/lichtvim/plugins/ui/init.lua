8f20e603c5dcd6d504fe8ccd32c54cc3017ef48d
暮晨 <leisurelicht@gmail.com>
2023-07-20 15:53:18 +0800

    feat(fold): move fold config to nvim-ufo init function

--- lua/lichtvim/plugins/ui/init.lua
+++ lua/lichtvim/plugins/ui/init.lua
@@ -110,6 +110,14 @@ return {
   { -- better fold
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
+    init = function()
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      vim.o.foldenable = true
+    end,
     opts = function()
       local handler = function(virtText, lnum, endLnum, width, truncate)
         local newVirtText = {}
