ef91c348962f8eb9fcdc573e792f06c9ea970a53
暮晨 <leisurelicht@gmail.com>
2023-07-20 16:16:45 +0800

    feat(nvim-ufo):disable nvim-ufo to test

--- lua/lichtvim/plugins/ui/init.lua
+++ lua/lichtvim/plugins/ui/init.lua
@@ -109,6 +109,7 @@ return {
   },
   { -- better fold
     "kevinhwang91/nvim-ufo",
+    enabled = false,
     dependencies = "kevinhwang91/promise-async",
     init = function()
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
