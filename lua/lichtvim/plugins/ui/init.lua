8b72b1d2006953d92f5b1fd4fb6668289ca2f4e9
暮晨 <leisurelicht@gmail.com>
2023-07-20 16:00:29 +0800

    refactor:删除重复代码

--- lua/lichtvim/plugins/ui/init.lua
+++ lua/lichtvim/plugins/ui/init.lua
@@ -116,7 +116,6 @@ return {
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
-      vim.o.foldenable = true
     end,
     opts = function()
       local handler = function(virtText, lnum, endLnum, width, truncate)
