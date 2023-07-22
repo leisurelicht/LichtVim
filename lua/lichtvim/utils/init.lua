9af136daca2a26866b9a87073fb03d8b2de0bf8c
leisurelicht <leisurelicht@gmail.com>
2023-07-23 01:01:34 +0800

    fix(util): fixed line number toggling

--- lua/lichtvim/utils/init.lua
+++ lua/lichtvim/utils/init.lua
@@ -308,6 +308,20 @@ function M.option.toggle_listchars()
   end
 end
 
+local nu = { number = true, relativenumber = true }
+function M.option.toggle_number()
+  if vim.opt_local.number:get() or vim.opt_local.relativenumber:get() then
+    nu = { number = vim.opt_local.number:get(), relativenumber = vim.opt_local.relativenumber:get() }
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    log.warn("Disabled line numbers", { title = "Option" })
+  else
+    vim.opt_local.number = nu.number
+    vim.opt_local.relativenumber = nu.relativenumber
+    log.info("Enabled line numbers", { title = "Option" })
+  end
+end
+
 M.plugs = {}
 
 --- This will return a function that calls telescope.
